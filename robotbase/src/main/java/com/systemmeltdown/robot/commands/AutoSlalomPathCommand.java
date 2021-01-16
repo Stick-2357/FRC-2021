464442767c3193ea85b693d9e62947bbefe5ac32
NC2357 <crazyfloob@gmail.com>
2021-01-16 15:03:18 -0600

    Update Slalom Path

--- robotbase/src/main/java/com/systemmeltdown/robot/commands/AutoSlalomPathCommand.java
+++ robotbase/src/main/java/com/systemmeltdown/robot/commands/AutoSlalomPathCommand.java
@@ -1,18 +1,16 @@
 package com.systemmeltdown.robot.commands;
 
-import com.systemmeltdown.robotlib.subsystems.drive.SkidSteerDriveSubsystem;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-import frc.robot.Constants;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
+import com.systemmeltdown.robotlib.subsystems.drive.FalconTrajectoryDriveSubsystem;
 
-public class AutoSlalomPathCommand extends ParallelRaceGroup{
-    private SkidSteerDriveSubsystem m_driveSub;
+public class AutoSlalomPathCommand extends SequentialCommandGroup{
+    private FalconTrajectoryDriveSubsystem m_driveSub;
     private double m_speed;
     private double m_turn;
     private double m_driveSeconds;
 
-    public AutoSlalomPathCommand(SkidSteerDriveSubsystem driveSubsystem) {
+    public AutoSlalomPathCommand(FalconTrajectoryDriveSubsystem driveSubsystem) {
     m_driveSub = driveSubsystem;
     //One revolution of six-inch wheel = 18.84 inches per revolution
     addCommands(
@@ -36,8 +34,9 @@ public class AutoSlalomPathCommand extends ParallelRaceGroup{
     new AutoEncoderDriveCommand(m_driveSub, 4.514, 26.331),
     //Forward 43.806”
     //Right 26.331”, left 4.514”
-    new AutoEncoderDriveCommand(m_driveSub, 26.331, 4.514);
+    new AutoEncoderDriveCommand(m_driveSub, 26.331, 4.514),
     //Forward to finish
+    new AutoEncoderDriveCommand(m_driveSub, 20, 20)
     );
     }
 }
