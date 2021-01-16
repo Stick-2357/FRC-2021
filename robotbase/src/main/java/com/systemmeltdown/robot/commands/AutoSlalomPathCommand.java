2b3ac89c7071085cda8427a4dc66197734697eab
NC2357 <crazyfloob@gmail.com>
2021-01-16 16:12:29 -0600

    Updates path.

--- robotbase/src/main/java/com/systemmeltdown/robot/commands/AutoSlalomPathCommand.java
+++ robotbase/src/main/java/com/systemmeltdown/robot/commands/AutoSlalomPathCommand.java
@@ -9,21 +9,35 @@ public class AutoSlalomPathCommand extends SequentialCommandGroup{
 
     public AutoSlalomPathCommand(FalconTrajectoryDriveSubsystem driveSubsystem) {
     m_driveSub = driveSubsystem;
-    //One revolution of six-inch wheel = 18.84 inches per revolution
+    
     addCommands(
-    //Forward 60” (center on edge), Right 26.331”, left 4.514”
+    //Forward 60” (center on edge)
+    new AutoEncoderDriveCommand(m_driveSub, 60, 60),
+    //Right 26.331”, left 4.514”
     new AutoEncoderDriveCommand(m_driveSub, 26.331, 4.514),
-    //Forward 43.806”, Right 4.514”, left 26.331”
+    //Forward 43.806”
+    new AutoEncoderDriveCommand(m_driveSub, 43.806, 43.806),
+    //Right 4.514”, left 26.331”
     new AutoEncoderDriveCommand(m_driveSub, 4.514, 25.331),
-    //Forward 120, Right 4.514”, left 26.331”
+    //Forward 120
+    new AutoEncoderDriveCommand(m_driveSub, 120, 120),
+    //Right 4.514”, left 26.331”
     new AutoEncoderDriveCommand(m_driveSub, 4.524, 26.331),
-    //Forward 43.806”, Right 162.618”, left 27.877”
+    //Forward 43.806”
+    new AutoEncoderDriveCommand(m_driveSub, 43.806, 43.806),
+    //Right 162.618”, left 27.877”
     new AutoEncoderDriveCommand(m_driveSub, 162.618, 27.877),
-    //Forward 43.806”, Right 4.514”, left 26.331”
+    //Forward 43.806”
+    new AutoEncoderDriveCommand(m_driveSub, 43.806, 43.806),
+    //Right 4.514”, left 26.331”
     new AutoEncoderDriveCommand(m_driveSub, 4.514, 26.331),
-    //Forward 120”, Right 4.514”, left 26.331”
+    //Forward 120”
+    new AutoEncoderDriveCommand(m_driveSub, 120, 120),
+    //Right 4.514”, left 26.331”
     new AutoEncoderDriveCommand(m_driveSub, 4.514, 26.331),
-    //Forward 43.806”, Right 26.331”, left 4.514”
+    //Forward 43.806”
+    new AutoEncoderDriveCommand(m_driveSub, 43.806, 43.806),
+    //Right 26.331”, left 4.514”
     new AutoEncoderDriveCommand(m_driveSub, 26.331, 4.514),
     //Forward to finish
     new AutoEncoderDriveCommand(m_driveSub, 20, 20)
