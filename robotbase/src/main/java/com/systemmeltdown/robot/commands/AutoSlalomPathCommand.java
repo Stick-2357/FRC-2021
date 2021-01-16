64e40b92fba47d7a534499c0ba5ca6d786ffd7d4
NC2357 <crazyfloob@gmail.com>
2021-01-16 16:21:36 -0600

    Switched left and right

--- robotbase/src/main/java/com/systemmeltdown/robot/commands/AutoSlalomPathCommand.java
+++ robotbase/src/main/java/com/systemmeltdown/robot/commands/AutoSlalomPathCommand.java
@@ -13,44 +13,44 @@ public class AutoSlalomPathCommand extends SequentialCommandGroup {
                 new AutoEncoderDriveCommand(driveSubsystem, 60, 60),
 
                 // Right 26.331”, left 4.514”
-                new AutoEncoderDriveCommand(driveSubsystem, 26.331, 4.514),
+                new AutoEncoderDriveCommand(driveSubsystem, 4.514, 26.331),
 
                 // Forward 43.806”
                 new AutoEncoderDriveCommand(driveSubsystem, 43.806, 43.806),
 
                 // Right 4.514”, left 26.331”
-                new AutoEncoderDriveCommand(driveSubsystem, 4.514, 25.331),
+                new AutoEncoderDriveCommand(driveSubsystem, 26.331, 4.514),
 
                 // Forward 120
                 new AutoEncoderDriveCommand(driveSubsystem, 120, 120),
 
                 // Right 4.514”, left 26.331”
-                new AutoEncoderDriveCommand(driveSubsystem, 4.524, 26.331),
+                new AutoEncoderDriveCommand(driveSubsystem, 26.331, 4.514),
 
                 // Forward 43.806”
                 new AutoEncoderDriveCommand(driveSubsystem, 43.806, 43.806),
 
                 // Right 162.618”, left 27.877”
-                new AutoEncoderDriveCommand(driveSubsystem, 162.618, 27.877),
+                new AutoEncoderDriveCommand(driveSubsystem, 27.877, 162.618),
 
                 // Forward 43.806”
                 new AutoEncoderDriveCommand(driveSubsystem, 43.806, 43.806),
 
                 // Right 4.514”, left 26.331”
-                new AutoEncoderDriveCommand(driveSubsystem, 4.514, 26.331),
+                new AutoEncoderDriveCommand(driveSubsystem, 26.331, 4.514),
 
                 // Forward 120”
                 new AutoEncoderDriveCommand(driveSubsystem, 120, 120),
 
                 // Right 4.514”, left 26.331”
-                new AutoEncoderDriveCommand(driveSubsystem, 4.514, 26.331),
+                new AutoEncoderDriveCommand(driveSubsystem, 26.331, 4.514),
 
                 // Forward 43.806”
                 new AutoEncoderDriveCommand(driveSubsystem, 43.806, 43.806),
 
                 // Right 26.331”, left 4.514”
-                new AutoEncoderDriveCommand(driveSubsystem, 26.331, 4.514),
-                
+                new AutoEncoderDriveCommand(driveSubsystem, 4.514, 26.331),
+
                 // Forward to finish
                 new AutoEncoderDriveCommand(driveSubsystem, 20, 20));
     }
